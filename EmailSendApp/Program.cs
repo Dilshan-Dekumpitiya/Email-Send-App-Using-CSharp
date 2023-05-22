95b6dc6cfd445225617cd0dbfa92bee8ccf7d9b1
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-05-22 15:06:23 +0530

    email change

---
+++ EmailSendApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Mail;
+using System.Configuration;
+using NLog;
+
+namespace EmailSendApp
+{
+    internal class Program
+    {
+
+        private static Logger logger = LogManager.GetLogger("nlog.config");
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+
+                SmtpClient smtpClient = new SmtpClient(GetConfigurationValue("smtpClient"));
+                mail.From = new MailAddress(GetConfigurationValue("mailFrom")); 
+                mail.To.Add(GetConfigurationValue("mailTo"));
+                mail.Subject = "Test";
+                mail.Body = "Test Body";
+                
+                logger.Info("Email is preparing...");
+                logger.Info(mail.Subject);
+                logger.Info(mail.Body);
+
+                smtpClient.Port = int.Parse(GetConfigurationValue("smtpClientPort"));
+                smtpClient.Credentials = new System.Net.NetworkCredential(GetConfigurationValue("smtpClientCredentialsUsername"), GetConfigurationValue("smtpClientCredentialsPassword"));
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mail);
+
+                logger.Info("Email sent successfully");
+
+            }
+           catch (Exception ex)
+            {
+                logger.Error(ex);
+
+            }
+        }
+
+        public static string GetConfigurationValue(string key)
+        {
+            return ConfigurationManager.AppSettings.Get(key) ?? "Default Value";
+        }
+    }
+}
+
